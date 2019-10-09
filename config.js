84d2f76c1a2dbf9f874036d890b490849fec9ede
jasminegmp <jasminekgump@gmail.com>
2019-10-09 15:11:26 -0700

    Imported gatsby-starter-spectral as starter code

---
+++ config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  siteTitle: 'Gatsby Starter Spectral', // <title>
+  manifestName: 'Spectral',
+  manifestShortName: 'Landing', // max 12 characters
+  manifestStartUrl: '/',
+  manifestBackgroundColor: '#663399',
+  manifestThemeColor: '#663399',
+  manifestDisplay: 'standalone',
+  manifestIcon: 'src/assets/img/website-icon.png',
+  pathPrefix: `/gatsby-starter-spectral/`, // This path is subpath of your hosting https://domain/portfolio
+  heading: 'Jasmine Gump',
+  subHeading: 'Web Developer. Artist. Avid Reader.',
+  // social
+  socialLinks: [
+    {
+      style: 'brands',
+      icon: 'fa-github',
+      name: 'Github',
+      url: 'https://github.com/jasminegmp',
+    },
+    {
+      style: 'brands',
+      icon: 'fa-twitter',
+      name: 'Twitter',
+      url: 'https://twitter.com/ladyjellington',
+    },
+    {
+      style: 'solid',
+      icon: 'fa-envelope',
+      name: 'Email',
+      url: 'mailto: jasminegmp@gmail.com',
+    },
+  ],
+};
